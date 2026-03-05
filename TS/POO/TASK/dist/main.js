a730c06c5ebff4f5ee98c69caed29502cba4339e
harounaKane <harouna.Kane@hotmail.fr>
2026-03-05 16:28:30 +0100

    task bis

--- TS/POO/TASK/dist/main.js
+++ TS/POO/TASK/dist/main.js
@@ -1,3 +1,6 @@
+import { PriorityTask } from "./models/PriorityTask.js";
+import { Task } from "./models/Task.js";
+import { TaskManager } from "./services/TaskManager.js";
 // const qs = <T>(sel: string): T => document.querySelector(sel) as T;
 function qs(sel) {
     const el = document.querySelector(sel);
@@ -9,4 +12,35 @@ let input = qs("#taskTitle");
 let select = qs("#priority");
 let ul = qs("#taskList");
 let btn = qs("#addTask");
-export {};
+const manager = new TaskManager();
+btn.addEventListener("click", () => {
+    let title = input.value;
+    let priority = parseInt(select.value);
+    const id = Math.floor(Math.random() * 10000 + 1000);
+    let task;
+    if (priority) {
+        task = new PriorityTask(id, title, false, priority);
+    }
+    else {
+        task = new Task(id, title, false);
+    }
+    manager.addTask(task);
+    input.value = "";
+    input.focus();
+    select.value = "";
+    render();
+});
+function render() {
+    let li = document.createElement("li");
+    manager.tasks.forEach(task => {
+        if (task instanceof PriorityTask) {
+            li.textContent = `⚠️ ${task.title} (priorité ${task.priority})`;
+        }
+        else {
+            li.textContent = task.title;
+        }
+        //    li.style.textDecoration = "line-through"
+        // li.addEventListener("click")
+    });
+    ul.appendChild(li);
+}
--- TS/POO/TASK/src/main.ts
+++ TS/POO/TASK/src/main.ts
@@ -16,3 +16,42 @@ let select:HTMLSelectElement = qs("#priority");
 let ul:HTMLUListElement = qs("#taskList");
 let btn:HTMLButtonElement = qs("#addTask");
 
+const manager = new TaskManager();
+
+btn.addEventListener("click", () => {
+    let title = input.value;
+    let priority = parseInt(select.value);
+
+    const id = Math.floor( Math.random() * 10000 + 1000 );
+    let task:Task;
+
+    if( priority ){
+        task = new PriorityTask(id, title, false, priority);
+    }else{
+        task = new Task(id, title, false);
+    }
+    
+    manager.addTask(task);
+
+    input.value = "";
+    input.focus();
+    select.value = "";
+
+    render();
+});
+
+function render() {
+    let li = document.createElement("li");
+    manager.tasks.forEach(task => {
+        if( task instanceof PriorityTask ){
+            li.textContent = `⚠️ ${task.title} (priorité ${task.priority})`;
+        }else{
+            li.textContent = task.title;
+        }
+
+    //    li.style.textDecoration = "line-through"
+       // li.addEventListener("click")
+    });
+
+    ul.appendChild(li);
+}
