b3e0ce423cbeada49e8a885570ef6e8ddfcda648
harounaKane <harouna.Kane@hotmail.fr>
2026-04-10 16:52:41 +0200

    node & react

--- NODEjs/institut/controllers/Stage.controller.js
+++ NODEjs/institut/controllers/Stage.controller.js
@@ -13,11 +13,41 @@ export const getStages = async (req, res) => {
 export const createStage = async (req, res) => {
   try {
     const { nom, debut, fin, description } = req.body;
-    console.log(req.body);
 
     const stage = await Stage.create({ nom, debut, fin, description });
 
     res.status(200).json(stage);
+  } catch (error) {
+    res.status(404).json({ erreur: error.message });
+  }
+};
+
+export const updateStage = async (req, res) => {
+  try {
+    const stage = await Stage.findByPk(req.params.id);
+
+    if (!stage)
+      return res.status(500).json({ erreur: "pas de stage avec cet id !" });
+    const { nom, debut, fin, description } = req.body;
+
+    await stage.update({ nom, debut, fin, description });
+
+    res.status(200).json(stage);
+  } catch (error) {
+    res.status(404).json({ erreur: error.message });
+  }
+};
+
+export const deleteStage = async (req, res) => {
+  try {
+    const stage = await Stage.findByPk(req.params.id);
+
+    if (!stage)
+      return res.status(500).json({ erreur: "pas de stage avec cet id !" });
+
+    await stage.destroy();
+
+    res.status(200).json(`le stage ${stage.nom} est supprimé !`);
   } catch (error) {
     res.status(404).json({ erreur: error.message });
   }
--- NODEjs/institut/routes/Stage.route.js
+++ NODEjs/institut/routes/Stage.route.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { createStage, getStages } from "../controllers/Stage.controller.js";
+import {
+  createStage,
+  deleteStage,
+  getStages,
+  updateStage,
+} from "../controllers/Stage.controller.js";
 
 const router = express.Router();
 
 router.get("/", getStages);
 router.post("/", createStage);
+router.delete("/:id", deleteStage);
+router.put("/:id", updateStage);
 
 export default router;
